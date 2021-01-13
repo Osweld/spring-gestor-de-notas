f8841a280fc8465768229bc1dc409e892d4e5dd7
osweld <oswaldo101.gr@gmail.com>
2021-01-12 18:02:46 -0600

    Creacion del proyecto

---
+++ src/main/java/com/osweld/dev/SpringGestorDeNotasApplication.java
@@ -0,0 +1,13 @@
+package com.osweld.dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGestorDeNotasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGestorDeNotasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/osweld/dev/SpringGestorDeNotasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.osweld.dev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGestorDeNotasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
