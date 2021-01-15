447d5755c568a945dcb2f34703b98cd694df3f42
osweld <oswaldo101.gr@gmail.com>
2021-01-14 21:06:51 -0600

    Se creo una clase que hereda de springSecurity.User para manejar los inicios de sesion

---
+++ src/main/java/com/osweld/dev/auth/user/AuthUser.java
@@ -0,0 +1,37 @@
+package com.osweld.dev.auth.user;
+
+import java.util.Collection;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+
+public class AuthUser extends User{
+	
+	private static final long serialVersionUID = 1L;
+	
+	private Long id;
+
+	public AuthUser(String username, String password, boolean enabled, boolean accountNonExpired,
+			boolean credentialsNonExpired, boolean accountNonLocked,
+			Collection<? extends GrantedAuthority> authorities) {
+		super(username, password, enabled, accountNonExpired, credentialsNonExpired, accountNonLocked, authorities);
+	}
+	
+	public AuthUser(Long id,String username, String password, boolean enabled, boolean accountNonExpired,
+			boolean credentialsNonExpired, boolean accountNonLocked,
+			Collection<? extends GrantedAuthority> authorities) {
+		super(username, password, enabled, accountNonExpired, credentialsNonExpired, accountNonLocked, authorities);
+		this.id = id;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	
+
+}
