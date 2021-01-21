77c46fe04d31baed465bd7fed6346264c4285f1f
osweld <oswaldo101.gr@gmail.com>
2021-01-21 10:03:08 -0600

    Se crearon los endpoints del userController

--- src/main/java/com/osweld/dev/controllers/RegistrationController.java
+++ src/main/java/com/osweld/dev/controllers/RegistrationController.java
@@ -8,13 +8,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -43,7 +41,7 @@ public class RegistrationController {
             User userResult = userService.saveUser(user);
             if(userResult != null){
                 body.put("message","El usuario a sido creado exitosamente");
-                body.put("user",user);
+                body.put("user",userResult);
                 responseEntity = new ResponseEntity<Map<String,Object>>(body,HttpStatus.OK);
             }else{
                 body.put("message","Hubo un error al crear el usuario");
@@ -58,5 +56,51 @@ public class RegistrationController {
         }
 
         return responseEntity;
+    }
+
+    @GetMapping("/one/{idUser}")
+    public ResponseEntity<Map<String,Object>> getUserById(@PathVariable Long idUser){
+        ResponseEntity<Map<String, Object>> responseEntity = null;
+        Map<String,Object> body = new HashMap<String,Object>();
+        try{
+            User userResult = userService.getUser(idUser);
+            if(userResult != null){
+                body.put("message","Se obtuvo el usuario exitosamente");
+                body.put("user",userResult);
+                responseEntity = new ResponseEntity<Map<String,Object>>(body,HttpStatus.OK);
+            }else{
+                body.put("message","No se pudo obtener el usuario");
+                body.put("error","usuario no encontrado");
+                responseEntity = new ResponseEntity<Map<String,Object>>(body,HttpStatus.INTERNAL_SERVER_ERROR);
+            }
+        }catch (DataAccessException e){
+            body.put("message","No se pudo obtener el usuario");
+            body.put("error","usuario no encontrado: "+e.getMostSpecificCause());
+            responseEntity = new ResponseEntity<Map<String,Object>>(body,HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        return responseEntity;
+    }
+
+    @GetMapping("/all/")
+    public ResponseEntity<Map<String,Object>> getAllUser(){
+        ResponseEntity<Map<String,Object>> responseEntity = null;
+        Map<String,Object> body = new HashMap<String,Object>();
+        try{
+            List<User> userList = userService.getAllUser();
+            if(userList.size() != 0){
+                body.put("message","Se obtuvieron los usuarios exitosamente");
+                body.put("users",userList);
+                responseEntity = new ResponseEntity<Map<String,Object>>(body,HttpStatus.OK);
+            }else{
+                body.put("message","No se pudieron obtener los usuarios");
+                body.put("error","No se han encontrado usuarios");
+                responseEntity = new ResponseEntity<Map<String,Object>>(body,HttpStatus.INTERNAL_SERVER_ERROR);
+            }
+        }catch(DataAccessException e){
+            body.put("message","No se pudieron obtener los usuarios");
+            body.put("error","No se han encontrado usuarios: "+e.getMostSpecificCause());
+            responseEntity = new ResponseEntity<Map<String,Object>>(body,HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        return null;
     }
 }
