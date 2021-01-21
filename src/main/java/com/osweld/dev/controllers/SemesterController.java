9cbf6ed7f7102e7903b2df8a1b5ae388e0b3faa2
osweld <oswaldo101.gr@gmail.com>
2021-01-21 10:30:35 -0600

    Se empezaron a crear los endpoints pero se va a continuar despues

--- src/main/java/com/osweld/dev/controllers/SemesterController.java
+++ src/main/java/com/osweld/dev/controllers/SemesterController.java
@@ -1,5 +1,26 @@
 package com.osweld.dev.controllers;
 
+import com.osweld.dev.services.SemesterService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/semester")
 public class SemesterController {
+
+    @Autowired
+    private SemesterService semesterService;
+
+    @PostMapping("/")
+    private ResponseEntity<Map<String,Object>> saveSemester(){
+
+        return null;
+    }
 
 }
